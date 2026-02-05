c623b55a4428cb6284e309bda958aff2c8bc6c9f
Abanee_ <146472520+Abanee@users.noreply.github.com>
2026-02-05 10:57:39 +0530

    Update Vite config to enable server settings
    Re-enable server configuration and comment out base URL.

--- Frnd/vite.config.js
+++ Frnd/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env,VITE_BASE || "/social_media_engagemant_analysis"
-  // server: {
-  //   port: 3000,
-  //   open: true
-  // }
+  // base: process.env,VITE_BASE || "/social_media_engagemant_analysis"
+  server: {
+    port: 3000,
+    open: true
+  }
 });
